e1311fc59aece8ce98d85e9fb254d7eb67a9654c
xiaobai <xiaobai-world@foxmail.com>
2021-05-27 23:09:33 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { IXiaobaiWorldMessageData, IWindow } from './types'
+
+export {
+ IXiaobaiWorldMessageData,
+ IWindow
+}
+
+/**
+* mount app to xiaobai or spa
+*/
+export function mount(): Promise<string> {
+ if (!window.xiaobaiApi) {
+  return Promise.reject()
+ }
+ return new Promise(function (resolve) {
+  function messageHandler(event: MessageEvent) {
+   const data: IXiaobaiWorldMessageData = event.data
+   if (data.XIAOBAI_EVENT !== 'XIAOBAI_APP_JAVASCRIPT_ENTRY_LOADED') {
+    return
+   }
+   const id = `#${data.id}`
+   if (document.querySelector(id)) {
+    window.removeEventListener('message', messageHandler)
+    resolve(id)
+   }
+  }
+  window.addEventListener('message', messageHandler)
+ })
+}
