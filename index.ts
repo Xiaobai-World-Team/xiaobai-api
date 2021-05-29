6404bb2566b7f339ee2cb95a0cf235b1daf2efc2
xiaobai <xiaobai-world@foxmail.com>
2021-05-30 01:14:49 +0800

    used relative path to take package info of xiaobai app

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import { IXiaobaiWorldMessageData, IWindow } from './types'
 
+/*
+ * because this module inside node_modules,
+ * so used relative path can visit package.json of xiaobai-app instance,
+ * will be bring typescript error, but on real enviorment no problem
+ */
+
+// @ts-ignore: Unreachable code error
+import * as appPackage from './../../../package.json';
+
 export type {
  IXiaobaiWorldMessageData,
  IWindow
@@ -21,7 +30,8 @@ export function mount(render: (selector: string) => void): Promise<void> {
    }
    const id = `#${data.id}`
    const node = document.querySelector(id);
-   if (node && node.getAttribute('app-name') === data.appName) {
+   const appName = appPackage.appName ? appPackage.appName : appPackage.name;
+   if (node && node.getAttribute('app-name') === appName) {
     render(id)
    }
   }
