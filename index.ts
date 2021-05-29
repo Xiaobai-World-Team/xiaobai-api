e973f77791ba6b8c8ac4cf85cb34eaedca4025eb
xiaobai <xiaobai-world@foxmail.com>
2021-05-29 12:32:29 +0800

    fix bug: the app was mounted to the wrong container

--- index.ts
+++ index.ts
@@ -13,15 +13,16 @@ export function mount(render: (selector: string) => void): Promise<void> {
   return Promise.reject()
  }
  return new Promise(function (resolve) {
+  resolve()
   function messageHandler(event: MessageEvent) {
    const data: IXiaobaiWorldMessageData = event.data
    if (data.XIAOBAI_EVENT !== 'XIAOBAI_APP_JAVASCRIPT_ENTRY_LOADED') {
     return
    }
    const id = `#${data.id}`
-   if (document.querySelector(id)) {
+   const node = document.querySelector(id);
+   if (node && node.getAttribute('app-name') === data.appName) {
     render(id)
-    resolve()
    }
   }
   window.addEventListener('message', messageHandler)
