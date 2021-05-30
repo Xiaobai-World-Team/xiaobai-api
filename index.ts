79a0ccc88ce83061c0faf065b3e9ed2b273b2063
xiaobai <xiaobai-world@foxmail.com>
2021-05-31 01:09:37 +0800

    integration various interface and install axios

--- index.ts
+++ index.ts
@@ -1,4 +1,103 @@
-import { IXiaobaiWorldMessageData, IWindow } from './types'
+import axios from 'axios'
+
+/** Xiaobai API */
+export interface IXiaobaiApi {
+ /* Show Popup Menu */
+ TrackPopupMenu(menus: IContextMenuItem[]): void
+ FileSystem: XiaobaiFileSystem
+}
+
+
+export interface IXiaobaiWorldMessageData {
+ XIAOBAI_EVENT: "XIAOBAI_APP_JAVASCRIPT_ENTRY_LOADED",
+ id: string,
+ name: string,
+ title: string,
+}
+
+/**
+ * Right-click menu item
+ */
+export interface IContextMenuItem {
+ /** union id */
+ id: string;
+ /** Make button gray */
+ disable: boolean;
+ /** Button icon */
+ icon?: string;
+ /* Button text */
+ text: string;
+ /** 
+  * Usually positioned to the right to display shortcuts
+  */
+ describe?: string;
+ /** callback */
+ callback(event: IContextMenuItem): any;
+ /** sub menu  */
+ child?: IContextMenuItem[],
+}
+
+/**
+ * Right-click menu
+ */
+export interface IContextMenu {
+ /** Whether the  "right click menu" is visible */
+ visible: boolean;
+ /** The x coordinate of the menu  */
+ x: number
+ /** The y coordinate of the menu */
+ y: number
+ /** Menu list */
+ menus: IContextMenuItem[]
+}
+
+
+declare global {
+ interface Window {
+  xiaobaiApi: IXiaobaiApi
+ }
+}
+
+/** 
+ * on desktop have mutiple windows, every window has below "IWindow interface",
+ * each window be the body's directly child
+ * */
+export interface IWindow {
+ /**
+  * unique id of window, global unique
+  */
+ id: string;
+ /** mount point node */
+ mountPointId: string;
+ /** 
+  * the name of the application mounted on the window, such as:
+  * notepad, photoPreview, Calendar, allow repetition in multiple windows
+  */
+ name: string;
+ title: string,
+ x: number
+ y: number
+ width: number
+ height: number
+ active: boolean
+ visible: boolean
+ icon: string
+ animation: boolean
+ /** before the window is maximized,save the window size for later recovery */
+ previousWindowSize?: {
+  x: number,
+  y: number,
+  width: number,
+  height: number
+ },
+ /** whether the window has been consumed */
+ isUsed: boolean,
+ /** auto start? */
+ autoStart: boolean,
+ jsEntry: string,
+ css: string[]
+}
+
 
 /*
  * because this module inside node_modules,
@@ -8,11 +107,6 @@ import { IXiaobaiWorldMessageData, IWindow } from './types'
 
 // @ts-ignore: Unreachable code error
 import * as appPackage from './../../../package.json';
-
-export type {
- IXiaobaiWorldMessageData,
- IWindow
-}
 
 /**
 * mount app to xiaobai or spa
@@ -30,11 +124,37 @@ export function mount(render: (selector: string) => void): Promise<void> {
    }
    const id = `#${data.id}`
    const node = document.querySelector(id);
-   const appName = appPackage.name;
-   if (node && node.getAttribute('app-name') === appName) {
+   const name = appPackage.name;
+   if (node && node.getAttribute('app-name') === name) {
     render(id)
    }
   }
   window.addEventListener('message', messageHandler)
  })
 }
+
+
+/**
+ * virtual disk api;
+ * avoid some users using cloud disk,because the priceis hard to bear. so matain target is save of small size file.
+ */
+
+
+/**
+ * file system
+ */
+export class XiaobaiFileSystem {
+ /** read dir file list */
+ async readDir(path: String) {
+  return (await axios.post('/storage/fileSystem/readDir', {
+   path
+  })).data.data
+ }
+ /** mkdir */
+ async mkDir(path: String) {
+  return (await axios.post('/storage/fileSystem/mkdir', {
+   path
+  })).data.data
+ }
+}
+
