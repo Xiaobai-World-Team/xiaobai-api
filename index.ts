be490d57558fd5f0a94f53a35fcb3ad05b30f447
xiaobai <xiaobai-world@foxmail.com>
2021-06-14 20:05:11 +0800

    add ts error handling

--- index.ts
+++ index.ts
@@ -129,8 +129,12 @@ export function mount(render: (selector: string) => void): Promise<void> {
    }
    const id = `#${data.id}`
    const node = document.querySelector(id);
-   const name = appPackage.name;
-   if (node && node.getAttribute('name') === name) {
+   // @ts-ignore: Unreachable code error
+   if (!appPackage.name) {
+    throw new Error('package.json must include name field')
+   }
+   // @ts-ignore: Unreachable code error
+   if (node && node.getAttribute('name') === appPackage.name) {
     render(id)
    }
   }
